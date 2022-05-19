059d8eef23dea9f9bbb98721638083f4dda60168
perry <phouchens@protonmail.com>
2022-05-19 09:04:44 -0400

    init

---
+++ init.lua
@@ -0,0 +1,23 @@
+require "options"
+require "keymaps"
+require "plugins"
+require "telescope-config"
+require "coc-config"
+
+require('lualine').setup()
+
+require'nvim-treesitter.configs'.setup {
+  context_commentstring = {
+    enable = true
+  },
+  highlight = {
+    enable = true,
+    disable = { "lua" }
+  },
+  indent = {
+    enable = true
+  }
+}
+
+
+vim.cmd "colorscheme sonokai"
